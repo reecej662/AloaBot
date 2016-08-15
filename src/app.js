c6bf9d122ca870f09769c4d8783d71aa3ef3b26b
reecej662 <reecej662@gmail.com>
2016-08-15 14:45:09 -0700

    Changed access token

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const REST_PORT = (process.env.PORT || 5000);
 const APIAI_ACCESS_TOKEN = process.env.APIAI_ACCESS_TOKEN;
 const APIAI_LANG = process.env.APIAI_LANG || 'en';
 const FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
-const FB_PAGE_ACCESS_TOKEN = process.env.FB_PAGE_ACCESS_TOKEN;
+const FB_PAGE_ACCESS_TOKEN = "aloalabs1";
 
 const apiAiService = apiai(APIAI_ACCESS_TOKEN, {language: APIAI_LANG, requestSource: "fb"});
 const sessionIds = new Map();
