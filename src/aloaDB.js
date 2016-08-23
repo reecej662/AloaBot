237bb6bcf08ee7194ad03cdcef854bcff8ab6c5d
reecej662 <reecej662@gmail.com>
2016-08-23 16:10:29 -0700

    Added sql.js

--- src/aloaDB.js
+++ src/aloaDB.js
@@ -40,7 +40,7 @@ module.exports.getProjects = function(client, completion) {
         if(err) throw err;
 
         for(var i in rows) {
-            result += "\nA " + rows[i].type + " project called " + rows[i].name + " for " + rows[i].client + "with a pay of " + rows[i].cost + "\n";
+            result += "\nA " + rows[i].type + " project called " + rows[i].name + " for " + rows[i].client + " with a pay of " + rows[i].cost + "\n";
         }
 
         console.log(result);
@@ -51,4 +51,15 @@ module.exports.getProjects = function(client, completion) {
     });
 
     return result;
+}
+
+module.exports.getUsers = function(language) {
+  var table = 'projects';
+  var connection = mysql.createConnection("mysql://b01d58c838662e:95af6763@us-cdbr-iron-east-04.cleardb.net/heroku_115917db4de1285?reconnect=true");
+  var sql = "SELECT fb_id FROM " + table + " WHERE fav_lang LIKE " + language;
+
+  connection.connect(function(err) {
+    if(err) throw err;
+    console.log("Connected to")
+  })
 }
--- src/sql.js
+++ src/sql.js
@@ -1,52 +1,20 @@
 const mysql = require('mysql');
-const REST_PORT = 5000;
-// var connection = mysql.createConnection({
-//   host     : 'localhost',
-//   user     : 'reece@aloalabs.com',
-//   password : 'norman323',
-//   database : 'projects',
-//   connectTimeout: 60000
-// });
-var table = 'projects'
+
+var query = ""
+
+if(process.argv.length > 2) {
+  query = process.argv[2];
+} else {
+  console.log("ERROR - Usage: node sql.js <SQL Query>");
+  return;
+}
+
+console.log("SQL Command: " + query);
 
 var connection = mysql.createConnection("mysql://b01d58c838662e:95af6763@us-cdbr-iron-east-04.cleardb.net/heroku_115917db4de1285?reconnect=true");
 
-connection.connect(function(err) {
-  if(err) throw err 
-  console.log("Connected to aloabot_db");
-});
-
-// var searchParam = 'fav_lang'
-// var langQuery = 'java';
-
-if(process.argv.length > 2) {
-  langQuery = process.argv[2];
-}
-
-var query = "SELECT * FROM " + table; // + " WHERE meta_key='" + searchParam + "' AND meta_value LIKE '%" + langQuery + "%'"
-console.log("SQL Command: " + query);
-
 connection.query(query, function(err, rows, fields) {
-
-  for(var i in rows) {
-    console.log(rows[i]);
+  if(err) {
+    console.log('Error in SQL command. Try again later');
   }
-
-  addNewProject('TestProject', 'TestClient', 'design', '1000');
 });
-
-function addNewProject(name, client, type, cost) {
-  var sql = "INSERT INTO projects SET ?";
-  var inserts = {'Name': name, 'Client': client, 'Type': type, 'Cost': cost};
-
-  connection.query(sql, inserts, function(err, result) {
-    if(err) {
-      throw err;
-    }
-
-    console.log(result);
-    connection.end();
-  });
-
-  console.log(query.sql);
-}
